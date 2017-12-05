90430d28034eb8a1213ae1a3945eeaffcfbeec42
chaitanya LN <chaituhaki@users.noreply.github.com>
2017-12-05 01:26:41 -0500

    added some more images

--- js/model.js
+++ js/model.js
@@ -8,5 +8,6 @@ var imageUrl = [
   "https://images.unsplash.com/photo-1481973964012-59a7f3225eb1?auto=format&fit=crop&w=1050&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
   "https://images.unsplash.com/photo-1497829352618-8528e15d7ce7?auto=format&fit=crop&w=1051&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
   "https://images.unsplash.com/photo-1465979058110-bb2dd732b99d?auto=format&fit=crop&w=1050&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
-  "https://images.unsplash.com/photo-1499107136159-5bd9740c34d8?auto=format&fit=crop&w=1050&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+  "https://images.unsplash.com/photo-1499107136159-5bd9740c34d8?auto=format&fit=crop&w=1050&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
+  "https://images.unsplash.com/photo-1507187632231-5beb21a654a2?auto=format&fit=crop&w=1077&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
 ];
