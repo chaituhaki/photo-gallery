cf4f5fcd0516b3659bed5c4b1a4eaafa8ac5c7b1
chaitanya LN <chaituhaki@users.noreply.github.com>
2017-12-18 12:05:20 -0500

    changed image height to vh removed window height event handler

--- js/view.js
+++ js/view.js
@@ -2,18 +2,6 @@ var thumbnails = document.getElementById("thumbnails");
 var image = document.getElementById("image");
 var left = document.getElementById("left");
 var right = document.getElementById("right");
-var windowHeight = window.innerHeight;
-
-// sets the image height to windows height
-image.style.height = windowHeight+"px";
-thumbnails.style.height = windowHeight-202.66+"px";
-//resize image if the window's height is chaged
-window.addEventListener("resize", function(){
-  windowHeight = window.innerHeight;
-  image.style.height = windowHeight+"px";
-  thumbnails.style.height = windowHeight-202.67+"px";
-  console.log(windowHeight-400+"px");
-});
 
 // populate thumbnails in side-menu
 for(i=0;i<imageUrl.length; i++){
